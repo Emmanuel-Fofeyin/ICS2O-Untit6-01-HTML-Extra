727b21b4a61cd482bea6f76e57135bb0ac4347b6
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2022-04-14 04:02:35 +0000

    fixed errors

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@
 // Created on: April 2022
 // This file contains the JS functions for index.html
 
-'use strict'
+"use·strict"
 /**
  * This function calculates volume of Sphere.
  */
 function calculate () {
   // input
-  const radius = parseInt(document.getElementById('radius').value)
+  const radius = parseInt(document.getElementById("radius").value)
 
   // process
   const volume = (4 / 3 * Math.PI * radius ** 3)
 
   // output
-  document.getElementById('area').innerHTML = 'The volume is: ' + volume.toFixed(2) + ' mm³'
+  document.getElementById("area").innerHTML = 'The volume is: ' + volume.toFixed(2) + ' mm³'
 }
